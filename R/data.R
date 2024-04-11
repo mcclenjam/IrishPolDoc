043350c48bc87267226e9107493bb8344250a199
mcclenjam <jamiemcclenaghan1999@gmail.com>
2024-04-11 08:25:52 +0100

    Initial commit

---
+++ R/data.R
@@ -0,0 +1,16 @@
+#' Data to showcase Irish internal party documents
+#'
+#' A dataset containing the internal policy documents of Irish political parties
+#'
+#' @format A data frame with 145 rows and 4 columns.
+#' \describe{
+#' \item{i}{ID for individual document}
+#' \item{text}{The text of the policy document}
+#' \item{document_name}{The name of the policy document}
+#' \item{party}{The political party to which the document belongs}
+#' }
+#' @source Created by Jamie McClenaghan by very unscientifically searching Irish political party websites for internal policy documents
+#'
+#' @examples
+#' data(IrishPolDocData)
+"IrishPolDocData"
